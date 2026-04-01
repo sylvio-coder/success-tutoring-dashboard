342c8fb15f285ce71f2e31885ff0292f69b8b3ec
sylvio-coder <sylvio@successtutoring.com>
2026-04-02 09:50:42 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1317,8 +1317,8 @@ def login_section():
         st.markdown(f'<p style="color:{BI_SUBTEXT};text-align:center;margin-bottom:24px">Sign in to continue</p>',
                     unsafe_allow_html=True)
         import urllib.parse
-        CLIENT_ID = "507985856717-srjmjg07sdde13anpr20io14ln46n9sf.apps.googleusercontent.com"
-        CLIENT_SECRET = "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"
+        CLIENT_ID = st.secrets.get("GOOGLE_CLIENT_ID", "")
+CLIENT_SECRET = st.secrets.get("GOOGLE_CLIENT_SECRET", "")
         REDIRECT_URI = "https://j7ky6kl5hwlbrjpxtuk8ce.streamlit.app/"
         AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
         TOKEN_URL = "https://oauth2.googleapis.com/token"
